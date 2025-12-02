379a6e994d98084fc426a57012a8bdb85434ec61
adipeled1 <adipeled10@gmail.com>
2025-12-02 21:59:20 +0200

    making TableReservation Serializable.

--- Assignment2/project Bistro9/bistro9-data/src/data/TableReservation.java
+++ Assignment2/project Bistro9/bistro9-data/src/data/TableReservation.java
@@ -1,76 +1,66 @@
 package data;
 
+import java.io.Serializable;
 
-public class TableReservation 
-{
+public class TableReservation implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private int reservationId;
-	private String  ReservationDate;
+	private String ReservationDate;
 	private int numberOfDiners;
 	private int confirmationCode;
 	private int subscriberId;
 	private String DateOfMakeReservation;
-	
-	public TableReservation() 
-	{
-		
-	}
-	
 
-	public int getReservationId() 
-	{
+	public TableReservation() {
+
+	}
+
+	public int getReservationId() {
 		return reservationId;
 	}
-	public void setReservationId(String reservationId) 
-	{
+
+	public void setReservationId(String reservationId) {
 		this.reservationId = Integer.parseInt(reservationId);
 	}
 
-
-	public String getReservationDate() 
-	{
+	public String getReservationDate() {
 		return ReservationDate;
 	}
-	public void setReservationDate(String reservationDate) 
-	{
+
+	public void setReservationDate(String reservationDate) {
 		ReservationDate = reservationDate;
 	}
 
-
-	public int getNumberOfDiners() 
-	{
+	public int getNumberOfDiners() {
 		return numberOfDiners;
 	}
-	public void setNumberOfDiners(String numberOfDiners) 
-	{
+
+	public void setNumberOfDiners(String numberOfDiners) {
 		this.numberOfDiners = Integer.parseInt(numberOfDiners);
 	}
 
-
-	public int getConfirmationCode() 
-	{
+	public int getConfirmationCode() {
 		return confirmationCode;
 	}
+
 	public void setConfirmationCode(String confirmationCode) {
 		this.confirmationCode = Integer.parseInt(confirmationCode);
 	}
 
-
-	public int getSubscriberId() 
-	{
+	public int getSubscriberId() {
 		return subscriberId;
 	}
-	public void setSubscriberId(String subscriberId) 
-	{
+
+	public void setSubscriberId(String subscriberId) {
 		this.subscriberId = Integer.parseInt(subscriberId);
 	}
 
-
-	public String getDateOfMakeReservation() 
-	{
+	public String getDateOfMakeReservation() {
 		return DateOfMakeReservation;
 	}
-	public void setDateOfMakeReservation(String dateOfMakeReservation) 
-	{
+
+	public void setDateOfMakeReservation(String dateOfMakeReservation) {
 		DateOfMakeReservation = dateOfMakeReservation;
 	}
 }
