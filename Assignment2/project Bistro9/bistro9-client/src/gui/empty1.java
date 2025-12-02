eeccef7ff2d8167db9cc5aca5f3d304a1fee032e
ororbach <or.orbach@e.braude.ac.il>
2025-12-02 12:26:09 +0200

    Changing packages

---
+++ Assignment2/project Bistro9/bistro9-client/src/gui/empty1.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class empty1 {
+
+	public empty1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Assignment2/project Bistro9/bistro9-server/src/gui/empty2.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class empty2 {
+
+	public empty2() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
